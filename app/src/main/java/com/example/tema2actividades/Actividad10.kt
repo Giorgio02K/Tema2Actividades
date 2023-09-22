7e9f05273023ff43daa1e93a08447db69f996fce
Giorgio02K <jordichorrog96@gmail.com>
2023-09-22 11:43:31 +0200

    Ejercicio10

--- app/src/main/java/com/example/tema2actividades/Actividad10.kt
+++ app/src/main/java/com/example/tema2actividades/Actividad10.kt
@@ -1,6 +1,15 @@
 package com.example.tema2actividades
 
 fun main() {
-
+    enteros(1,2,3,4,5,6)
 }
 
+fun enteros(vararg numeros: Int) {
+    var suma = 0
+    for (numero in numeros) {
+        println(numero)
+        suma += numero
+    }
+
+    println("La suma total es "+suma)
+}
